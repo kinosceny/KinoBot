19a0bf9a0e28f6bac6eafe3c51c68122047bfcde
semen-bol <syoma.bolotov@bk.ru>
2025-08-14 00:51:23 +0500

    feat: default message edited; fix: gramatic error;

--- bot/keyboards/keyboards.py
+++ bot/keyboards/keyboards.py
@@ -16,7 +16,7 @@ def main_kb(user_telegram_id: int):
 
 def admin_kb():
     kb_list = [
-        [KeyboardButton(text="Приветсвие"), KeyboardButton(text="Кнопки"), KeyboardButton(text="Остановить бота")],
+        [KeyboardButton(text="Приветствие"), KeyboardButton(text="Кнопки"), KeyboardButton(text="Остановить бота")],
         [KeyboardButton(text="Создать новый фильм"), KeyboardButton(text="Все фильмы"), KeyboardButton(text="Изменить цену фильма")],
         [KeyboardButton(text="Назад")]
     ]
